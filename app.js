459da68b462bff8b10720ecd112b75a9562a2a00
Saketh <sandragirisaki2004@gmail.com>
2024-02-26 09:47:57 +0530

    sd

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 app.use(cors())
+
 app.use(bodyParser.json());
+
+
+
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,13 +26,10 @@ app.use((req, res, next) => {
 app.use('/api/places', placesRoutes); 
 app.use('/api/users', usersRoutes);
 
-
 app.use((req, res, next) => {
-  // console.log(error)
   const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
-
 app.use((error, req, res, next) => {
   if(req.file){
     fs.unlink(req.file.path, err => {
@@ -41,6 +42,8 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500)
   res.json({message: error.message || 'An unknown error occurred!'});
 });
+
+
 
 mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lzodugo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
