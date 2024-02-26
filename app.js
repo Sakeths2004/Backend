944925f2b0750ee18319c77c3daefe98d6f5212c
Saketh <sandragirisaki2004@gmail.com>
2024-02-26 09:42:59 +0530

    sd

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.use((req, res, next) => {
 app.use('/api/places', placesRoutes); 
 app.use('/api/users', usersRoutes);
 
+
+app.use((req, res, next) => {
+  // console.log(error)
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
 app.use((error, req, res, next) => {
   if(req.file){
     fs.unlink(req.file.path, err => {
@@ -33,12 +40,6 @@ app.use((error, req, res, next) => {
   }
   res.status(error.code || 500)
   res.json({message: error.message || 'An unknown error occurred!'});
-});
-
-app.use((req, res, next) => {
-  // console.log(error)
-  const error = new HttpError('Could not find this route.', 404);
-  throw error;
 });
 
 mongoose
